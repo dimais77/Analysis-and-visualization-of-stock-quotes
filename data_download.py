5992ae4ff637837dfcc6eccd2e91f4d73bfdac16
Dmitrii Isaev <dimais.mail.ru@gmail.com>
2024-04-16 23:16:55 +0700

    Added error handling blocks (try-except)

--- data_download.py
+++ data_download.py
@@ -2,7 +2,6 @@ import yfinance as yf
 
 
 def fetch_stock_data(ticker, period='1mo', start=None):
-    # Функция отвечает за загрузку данных об акциях за период из yfinance.
     try:
         if start is not None:
             stock = yf.Ticker(ticker)
@@ -18,38 +17,45 @@ def fetch_stock_data(ticker, period='1mo', start=None):
 
 
 def add_moving_average(data, window_size=5):
-    # Функция отвечает за  расчёт скользящего среднего цены закрытия акций
-    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
-    return data
+    try:
+        data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+        return data
+    except Exception as e:
+        print(f"\nОшибка при добавлении скользящего среднего: {e}")
+        return None
 
 
 def calculate_and_display_average_price(data, ticker):
-    # Функция вычисляет и выводит в консоль среднюю цену закрытия акций за заданный период.
+    try:
+        avr_price = data['Close'].mean()
+        avr_price_rounded = round(avr_price, 6)
 
-    avr_price = data['Close'].mean()
-    avr_price_rounded = round(avr_price, 6)
+        start_date = data.index.min().strftime('%Y-%m-%d')
+        end_date = data.index.max().strftime('%Y-%m-%d')
 
-    start_date = data.index.min().strftime('%Y-%m-%d')
-    end_date = data.index.max().strftime('%Y-%m-%d')
-
-    print(
-        f'\nСредняя цена закрытия акции "{ticker}" за период <{start_date}...{end_date}> '
-        f'составила: {avr_price_rounded}')
+        print(
+            f'\nСредняя цена закрытия акции "{ticker}" за период <{start_date}...{end_date}> '
+            f'составила: {avr_price_rounded}')
+    except Exception as e:
+        print(f"\nОшибка при вычислении и отображении средней цены: {e}")
 
 
 def notify_if_strong_fluctuations(data, ticker, threshold=5):
-    # Функция вычисляет максимальное и минимальное значения цены закрытия и сравнивает с заданным порогом
-    # (по умолчанию порог threshold=5). Если разница превышает порог, пользователь получит уведомление.
-    max_closing_price = data['Close'].max()
-    min_closing_price = data['Close'].min()
-    fluctuation = ((max_closing_price - min_closing_price) / min_closing_price) * 100
-    fluctuation_rounded = round(fluctuation, 1)
-    if fluctuation_rounded > threshold:
-        print(
-            f'За указанный период отмечается сильное колебание цены закрытия акций "{ticker}": {fluctuation_rounded}%')
+    try:
+        max_closing_price = data['Close'].max()
+        min_closing_price = data['Close'].min()
+        fluctuation = ((max_closing_price - min_closing_price) / min_closing_price) * 100
+        fluctuation_rounded = round(fluctuation, 1)
+        if fluctuation_rounded > threshold:
+            print(
+                f'За указанный период отмечается сильное колебание цены закрытия акций "{ticker}": {fluctuation_rounded}%')
+    except Exception as e:
+        print(f"\nОшибка при определении сильных колебаний: {e}")
 
 
 def export_data_to_csv(data, filename):
-    #  Функция записывает данные в CSV файл
-    data.to_csv(filename)
-    print(f"\nДанные по запросу сохранены в файл {filename}")
+    try:
+        data.to_csv(filename)
+        print(f"\nДанные по запросу сохранены в файл {filename}")
+    except Exception as e:
+        print(f"\nОшибка при экспорте данных в CSV: {e}")
--- data_plotting.py
+++ data_plotting.py
@@ -3,29 +3,32 @@ import pandas as pd
 
 
 def create_and_save_plot(data, ticker, period, filename=None):
-    plt.figure(figsize=(10, 6))
+    try:
+        plt.figure(figsize=(10, 6))
 
-    if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
-            dates = data.index.to_numpy()
-            plt.plot(dates, data['Close'].values, label='Close Price')
-            plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
+        if 'Date' not in data:
+            if pd.api.types.is_datetime64_any_dtype(data.index):
+                dates = data.index.to_numpy()
+                plt.plot(dates, data['Close'].values, label='Close Price')
+                plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
+            else:
+                print("\nИнформация о дате отсутствует или не имеет распознаваемого формата.")
+                return
         else:
-            print("\nИнформация о дате отсутствует или не имеет распознаваемого формата.")
-            return
-    else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['Close'], label='Close Price')
-        plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
+            if not pd.api.types.is_datetime64_any_dtype(data['Date']):
+                data['Date'] = pd.to_datetime(data['Date'])
+            plt.plot(data['Date'], data['Close'], label='Close Price')
+            plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
 
-    plt.title(f"{ticker} Цена акций с течением времени")
-    plt.xlabel("Дата")
-    plt.ylabel("Цена")
-    plt.legend()
+        plt.title(f"{ticker} Цена акций с течением времени")
+        plt.xlabel("Дата")
+        plt.ylabel("Цена")
+        plt.legend()
 
-    if filename is None:
-        filename = f"{ticker}_{period}_stock_price_chart.png"
+        if filename is None:
+            filename = f"{ticker}_{period}_stock_price_chart.png"
 
-    plt.savefig(filename)
-    print(f"График сохранен как {filename}")
+        plt.savefig(filename)
+        print(f"График сохранен как {filename}")
+    except Exception as e:
+        print(f"\nОшибка при создании и сохранении графика: {e}")
